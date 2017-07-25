9a516ab1dc80bee3b96e788140935c9fc6a2b8b5
Gabi Nir <gabinir@gmail.com>
2017-07-26 01:53:21 +0300

    fixed tests

--- test/asyncStorage.js
+++ test/asyncStorage.js
@@ -1,8 +1,9 @@
 export default obj => ({
 	getItem: async (key) => obj[key],
 	setItem: async (key, item) => {
-		let resolve = null
-		const p = new Promise(res => resolve = res)
 		obj[key] = item
+	},
+	removeItem: async (key) => {
+		obj[key] = undefined
 	}
 })

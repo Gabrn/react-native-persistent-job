5d80e3716746d204a27c292f37dd9b74acb43857
Gabi Nir <gabinir@gmail.com>
2017-08-02 01:22:26 +0300

    changed numbers in withBackoff.spec a bit

--- test/subjectModifiers/retryStream/withBackoff.spec.js
+++ test/subjectModifiers/retryStream/withBackoff.spec.js
@@ -13,17 +13,17 @@ describe('Jobs run only when the device is online', () => {
 		stream.next({}) // 10 sec delay
 		await sleep(5)
 		expect(spy.mock.calls.length).toBe(0)
-		await sleep(6)
+		await sleep(11 - 5)
 		expect(spy.mock.calls.length).toBe(1)
 		stream.next({retryNumber: 1}) // 20 sec delay
-		await sleep(11)
+		await sleep(15)
 		expect(spy.mock.calls.length).toBe(1)
-		await sleep(10)
+		await sleep(21 - 15)
 		expect(spy.mock.calls.length).toBe(2)
 		stream.next({retryNumber: 3}) // 50 sec delay
 		await sleep(45)
 		expect(spy.mock.calls.length).toBe(2)
-		await sleep(6)
+		await sleep(51 - 45)
 		expect(spy.mock.calls.length).toBe(3)
 	})
 })

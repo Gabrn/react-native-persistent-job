5e28aff008a16b97f6d2084386cf5672f3cde029
Oren Kleinbaum <oren@soluto.com>
2018-01-09 15:30:34 +0200

    runWhenOnline - use new netinfo api

--- lib/streamModifiers/runWhenOnline.ts
+++ lib/streamModifiers/runWhenOnline.ts
@@ -1,21 +1,21 @@
-import {NetInfo} from 'react-native'
-import {Observable} from 'rxjs'
-import {JobNumbered} from '../jobTypes'
+import { NetInfo } from 'react-native'
+import { Observable } from 'rxjs'
+import { JobNumbered } from '../jobTypes'
 
 const runWhenOnline = (netInfo: NetInfo) => (subject: Observable<JobNumbered>) => {
-	const netInfoObservable: Observable<boolean> = Observable.fromEventPattern(h => netInfo.isConnected.addEventListener('change', <(r: boolean) => void>h))
-  const conectivityObservable: Observable<boolean> = Observable.merge(
-    Observable.fromPromise(netInfo.isConnected.fetch()),
-    netInfoObservable
- )
+	const netInfoObservable: Observable<boolean> = Observable.fromEventPattern(h => netInfo.isConnected.addEventListener('connectionChange', <(r: boolean) => void>h))
+	const connectivityObservable: Observable<boolean> = Observable.merge(
+		Observable.fromPromise(netInfo.isConnected.fetch()),
+		netInfoObservable
+	)
 
 	const obs = Observable
-		.combineLatest(subject, conectivityObservable, (job, isConnected) => ({job, isConnected}))
+		.combineLatest(subject, connectivityObservable, (job, isConnected) => ({ job, isConnected }))
 		.flatMap(
-			({job, isConnected}) => 
-				isConnected ? 
-					Observable.of(job) : 
-					netInfoObservable.filter(connectionIsBack => connectionIsBack).first().map(_ => job)
+		({ job, isConnected }) =>
+			isConnected ?
+				Observable.of(job) :
+				netInfoObservable.filter(connectionIsBack => connectionIsBack).first().map(_ => job)
 		)
 		.distinct(job => job.id)
 
